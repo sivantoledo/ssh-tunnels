5f161665b81d73d06a500361f9c321c4313e5690
Sivan Toledo <stoledo@mail.tau.ac.il>
2023-04-12 11:54:45 +0300

    create a zip as well as tgz, and add loop.sh

---
+++ loop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+while :
+do
+  java -jar sivantoledo.iot-1.0-jar-with-dependencies.jar listen
+  sleep 10
+done
--- pack.sh
+++ pack.sh
@@ -2,9 +2,13 @@
 
 filename=tunnels.tgz
 
-echo DISTRIBUTION FILENAME WILL BE $filename
+#echo DISTRIBUTION FILENAME WILL BE $filename
+
+echo GIT PULL
 
 git pull
+
+echo UPDATE DATE AND VERSION NUMBER AND COMMIT
 
 v=`cat version.txt`;
 echo CURRENT VERSION IS $v
@@ -17,6 +21,8 @@ git add date.txt version.txt
 git commit -m "packing a distribution"
 git push
 
+echo PACKING TGZ
+
 # tar zcvpf $filename --dereference --exclude='.git' --exclude '*~' jars tunnel.sh tunnel.bat version.txt date.txt gawk.exe
 
 tar cvpf tunnels.tar --dereference --exclude='.git' --exclude '*~' tunnel.sh tunnel.bat loop.sh version.txt date.txt system.txt
@@ -27,7 +33,21 @@ rm -f tunnels.tar.gz tunnels.tgz
 gzip tunnels.tar
 mv tunnels.tar.gz tunnels.tgz
 
-ls -l $filename
+echo CONTENTS OF TGZ FILE
+tar ztf tunnels.tgz
+
+echo PACKING ZIP
+
+rm -f tunnels.zip
+zip --junk-paths tunnels.zip tunnel.sh tunnel.bat loop.sh version.txt date.txt system.txt target/sivantoledo.iot-1.0-jar-with-dependencies.jar
+
+echo CONTENTS OF ZIP FILE
+
+unzip -l tunnels.zip
+
+echo ARCHIVE FILES:
+
+ls -l tunnels.*
 
 echo DONE
 
