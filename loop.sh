95639e2ee6b880a9385df6180c0f4a863d22e081
Sivan Toledo <stoledo@tau.ac.il>
2023-04-15 14:48:48 +0300

    redirect to tunnel.sh listen

--- loop.sh
+++ loop.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-while :
-do
-  java -jar sivantoledo.iot-1.0-jar-with-dependencies.jar listen
-  sleep 10
-done
+
+#while :
+#do
+#  java -jar sivantoledo.iot-1.0-jar-with-dependencies.jar listen
+#  sleep 10
+#done
+
+./tunnel.sh listen
